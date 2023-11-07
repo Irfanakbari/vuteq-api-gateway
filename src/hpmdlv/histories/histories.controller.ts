71c970086f22b99c7d84511e75917496c5e2637e
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-11-07 13:03:50 +0700

    history params fix

--- src/hpmdlv/histories/histories.controller.ts
+++ src/hpmdlv/histories/histories.controller.ts
@@ -35,6 +35,6 @@ export class HistoriesController {
     @Query('page') pageNumber?: number,
     @Query('limit') pageSize?: number,
   ) {
-    return this.historiesService.findAll(pageNumber ?? 1, pageSize ?? 100);
+    return this.historiesService.findAll(pageNumber ?? 0, pageSize ?? 100);
   }
 }
